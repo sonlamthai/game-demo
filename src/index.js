1c1f6a2d2be4fbf1128cf0cd3fec82d112af8b78
lamthaison <sonlamthai.vn>
2019-12-04 12:59:33 +0700

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         physics: {
             default: 'arcade',
             arcade: {
-                gravity: { y: 500 },
+                gravity: { y: 300 },
                 debug: false
             }
         },
@@ -22,14 +22,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var player;
     var platforms;
     var trees;
+    var cactus;
     var cursors;
     var score = 0;
+    var scoreText;
+    var click;
 
     var game = new Phaser.Game(config);
 
     function preload() {
         this.load.image('background', 'src/assets/BG.png');
-        // this.load.image('cactus','src/assets/Cactus (2).png');
+        this.load.image('cactus', 'src/assets/Cactus (2).png');
         this.load.image('tile', 'src/assets/Tile (13).png');
         this.load.image('tree', 'src/assets/Tree.png');
         this.load.spritesheet('dude', 'src/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
@@ -42,15 +45,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
         this.add.image(200, 300, 'background');
 
         platforms = this.physics.add.staticGroup();
-        platforms.create(128, 700, 'tile').setScale(6, 1).refreshBody();
+        platforms.create(128, 600, 'tile').setScale(6, 1).refreshBody();
 
-        platforms.create(675, 390, 'tile');
-        platforms.create(128, 245, 'tile');
+        platforms.create(675, 400, 'tile');
+        platforms.create(128, 325, 'tile');
 
         player = this.physics.add.sprite(100, 450, "dude");
 
         player.setBounce(0.5);
         player.setCollideWorldBounds(true);
+
+        cactus = this.physics.add.group({
+            key: 'cactus',
+            repeat: 8,
+            setXY: { x: 12, y: 0, stepX: 100 }
+        });
+
+        cactus.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.5, 0.75));
+
+        });
+
+        // cactus.setCollideWorldBounds(true);
+
+
+
+
 
         this.anims.create({
             key: 'left',
@@ -61,22 +81,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
+            frames: [{ key: 'dude', frame: 4 }],
             frameRate: 20
         });
-        
+
         // player.anims.play('turn');
-        
+
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
-        
+
         cursors = this.input.keyboard.createCursorKeys();
+        
+        
 
         this.physics.add.collider(player, platforms);
+        this.physics.add.collider(cactus, platforms);
+
+
+        this.physics.add.overlap(player, cactus, collectCactus, null, this);
+
+        scoreText = this.add.text(320, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+       
+
+
+
+
 
 
 
@@ -85,29 +119,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
 
-    function update () {
+    function update() {
 
         if (cursors.left.isDown) {
-            player.setVelocityX(-160);
+            player.setVelocityX(-600);
 
             player.anims.play('left', true);
         }
 
         else if (cursors.right.isDown) {
-            player.setVelocityX(160);
+            player.setVelocityX(600);
 
             player.anims.play('right', true);
         }
+
+        else if (cursors.up.isDown) {
+            player.setVelocityY(-300);
+        }
+
+        else if (cursors.down.isDown) {
+            player.setVelocityY(300)
+        }
+
         else {
 
             player.setVelocityX(0);
+            player.setVelocityY(0);
             player.anims.play('turn');
         }
 
-        if (cursors.up.isDown && player.body.touching.down) {
-            player.setVelocityY(-600);
-        }
 
+    }
+
+
+    function collectCactus(player, cactus) {
+        cactus.disableBody(true, true);
+        score += 1;
+        scoreText.setText('Score: ' + score);
     }
 
 
